fb5278287b251a35ff9df7efc79b462df10527b3
Razvan Botea <razvan@appscend.com>
2015-03-19 15:34:04 +0200

    implemented most model methods

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
- module.exports.Application = require('./lib/Application');
- module.exports.Event = require('./lib/Event');
+var fs = require('fs');
+
+module.exports.Application = require('./lib/Application');
+module.exports.Application.load();
+module.exports.Context = require('./lib/Context');
+module.exports.Context.load();
+module.exports.Model = require('./lib/Model');
+module.exports.getModels = function() {
+	var models = {};
+	var files = fs.readdirSync('./node_modules/octopus-models-api/models');
+
+	for (var f in files) {
+		if (files.hasOwnProperty(f)) {
+			var name = files[f].replace('.json', '');
+			models[name] = require('./models/'+files[f]);
+		}
+	}
+};
