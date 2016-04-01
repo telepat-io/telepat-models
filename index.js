9d61a6be29a378692d3c795fc5eb6abfa6f92dfb
Razvan Botea <razvan@appscend.com>
2016-04-01 15:00:29 +0300

    Improved documentation + code cleanup

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var fs = require('fs');
 
 module.exports.Application = require('./lib/Application');
-module.exports.Application.load();
 
 module.exports.Context = require('./lib/Context');
-module.exports.Context.load();
 
 module.exports.Model = require('./lib/Model');
 
 module.exports.Subscription = require('./lib/Subscription');
 
 module.exports.Admin = require('./lib/Admin');
-module.exports.Admin.load();
 
 module.exports.User = require('./lib/User');
-module.exports.User.load();
 
 module.exports.utils = require('./utils/utils');
 module.exports.Builders = require('./utils/filterbuilder');
